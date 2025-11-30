4b83391281ec48841e53631c1272e91093d982d3
Arun Tekwani <aruntekwani4567@gmail.com>
2025-11-30 13:07:51 +0530

    datatypes ki training

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+const str = "String h"
+
+console.log(str)
+console.log(typeof str)
+
+// string to number
+console.log(Number(str)) //Nan aayega
+console.log(Number("6")) // 6 as number
+
+// number to string
+console.log((6).toString()) // string me 6
+
+// boolean
+
+const bool = true
+console.log(bool)
+
+// null
+const nullType = null
+console.log(nullType)
+
+//undefined
+let undefinedType;
+console.log(undefinedType)
